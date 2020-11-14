5123f8addc589dd5f23107b0a1a9f13396010aa4
DioiD-PissMan <73759766+DioiD-PissMan@users.noreply.github.com>
2020-11-14 01:20:22 +0000

    Some changes
    Cavemen now exist, some minor changes too

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "The birth, of dogshit",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
